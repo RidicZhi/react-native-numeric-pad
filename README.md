467316e33d1f278bdbb0bf2fbe74739a6dc98d5e
zhi <haotong.zhi@lifepay.com.au>
2022-08-21 16:57:57 +1000

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,221 @@
+
+# React Native Numeric Pad
+
+  
+
+A React Native component for amount or verification code input. It easily handles both decimals and integers, and runs smoothly for both IOS and Android. The design is simple and clear with numbers, dot, and one custom button.
+
+  
+
+## Getting Started
+
+  
+
+**via Yarn**
+
+  
+
+```
+
+yarn add react-native-numeric-pad
+
+```
+
+  
+
+**via NPM**
+
+  
+
+```
+
+npm install --save react-native-numeric-pad
+
+```
+
+  
+
+## Usage
+
+  
+```
+
+import  NumericPad  from  'react-native-numeric-pad'
+
+...
+
+<NumericPad
+
+	numLength={8}
+
+	onValueChange={()  => {}}
+
+	allowDecimal={true}
+
+}/>
+
+```
+
+
+## Props
+
+| Prop                          | Type              | Default                                                                                                |  Required  |
+| ----------------------------- | ----------------- | ------------------------------------------------------------------------------------------------------ | ---------- |
+|numLength                      |number             |-                                                                                                       | **Yes**    |
+|onValueChange                  |func               |-                                                                                                       | No         |
+|activeOpacity                  |number             |`0.9`                                                                                                   | No         |
+|buttonSize                     |number             |`60`                                                                                                    | No         |
+|allowDecimal                   |boolean            |`true`                                                                                                  | No         |
+|style                          |ViewStyle          |-                                                                                                       | No         |
+|buttonAreaStyle                |ViewStyle          |-                                                                                                       | No         |
+|buttonItemStyle                |ViewStyle          |-                                                                                                       | No         |
+|buttonTextStyle                |TextStyle          |` { color: '#000', fontSize: 30, fontWeight: '400' }`                                                   | No         |
+|numericDisabled                |boolean            |-                                                                                                       | No         |
+|accessible                     |boolean            |`false`                                                                                                 | No         |
+|buttonTextByKey                |object             |`{one: "1",two: "2",three: "3",four: "4",five: "5",six: "6",seven: "7",eight: "8",nine: "9",dot: '.',zero: "0",}`| No|
+|rightBottomButton              |React.Component    |-                                                                                                       | No         |
+|onRightBottomButtonPress       |func               |-                                                                                                       | No         |
+|rightBottomButtonDisabled      |boolean            |-                                                                                                       | No         |
+|rightBottomButtonSize          |number             |`60`                                                                                                    | No         |
+|rightBottomAccessibilityLabel  |string             |`right_bottom`                                                                                          | No         |
+|rightBottomButtonItemStyle     |ViewStyle          |-                                                                                                       | No         |
+
+
+  
+
+## Ref Actions
+
+`const numpadRef = useRef(null)`
+
+  
+
+| Prop | Description |
+
+| ----------------------------- | -----------------------------------------------------|
+
+|numpadRef.current.clearAll() |This method completely clears the entered code. |
+
+|numpadRef.current.clear() |This method only delete last number of entered code. |
+
+  
+
+#### My Example
+
+  ![alt text](./numeric_pad_example.png 'Demo')
+
+```javascript
+
+import  React, { useState, useRef } from  'react'
+import { StyleSheet, Text, View, TextInput } from  'react-native'
+import { Button } from  '@ant-design/react-native'
+import { Ionicons } from  '@expo/vector-icons'
+import  NumericPad  from  'react-native-numeric-pad'
+import { I18n } from  '../../i18n'
+import { LAYOUT, COLORS } from  '../../theme'
+import { deviceHeight, deviceWidth } from  '../../theme/devices'
+
+export  default  function  Widget ({ navigation, route }) {
+const [amount, setAmount] = useState('')
+const  numpadRef = useRef(null)
+
+return (
+<View  style={styles.container}>
+	<View  style={styles.shadowBox  }>
+		<Text>{I18n.translate('amount')} $</Text>
+		<TextInput
+			style={styles.amountTxt}
+			showSoftInputOnFocus={false}
+			maxLength={8}
+			autoFocus={true}
+			editable={false}
+			selectTextOnFocus={false}
+			value={amount}
+			/>
+		</View>
+
+	<View  style={styles.keyboardContainer}>
+		<Button  style={styles.btn}  type='primary'
+			onPress={() => {}}
+			disabled={!amount}>
+			{I18n.translate('confirm') + ' →'}
+		</Button>
+
+		<NumericPad
+			ref={numpadRef}
+			numLength={8}
+			buttonSize={60}
+			activeOpacity={0.1}
+			onValueChange={value  =>  setAmount(value)}
+			allowDecimal={true}
+			// style={{ backgroundColor: 'black', paddingVertical: 12 }}
+			// buttonAreaStyle={{ backgroundColor: 'gray' }}
+			// buttonItemStyle={{ backgroundColor: 'red' }}
+			rightBottomButton={<Ionicons  name={'ios-backspace-outline'}  size={28}  color={'#000'}  />}
+			onRightBottomButtonPress={ () => { numpadRef.current.clear() }
+			}
+		/>
+	</View>
+</View>
+)
+}
+
+
+const  styles = StyleSheet.create({
+	container: {
+		flex:  1,
+		padding:  LAYOUT['spacing-05']
+		},
+		shadowBox: {
+		width:  '100%',
+		borderRadius:  LAYOUT['spacing-03'],
+		padding:  LAYOUT['spacing-05'],
+		backgroundColor:  COLORS.WHITE,
+		shadowOffset: {
+		height:  3,
+		width:  3
+		},
+		shadowOpacity:  0.15,
+		shadowRadius:  5,
+		shadowColor:  '#1D2660',
+		elevation:  5
+		},
+		amountTxt: {
+		fontSize:  38,
+		fontWeight:  '700',
+		lineHeight:  40,
+		marginTop:  LAYOUT['spacing-06'],
+		color:  COLORS['brand-01']
+	},
+	keyboardContainer: {
+		width:  deviceWidth,
+		height:  deviceHeight * 0.46,
+		borderRadius:  26,
+		justifyContent:  'center',
+		alignItems:  'center',
+		paddingHorizontal:  16,
+		position:  'absolute',
+		bottom:  0,
+		backgroundColor:  COLORS.WHITE,
+		shadowOffset: {
+		height:  3,
+		width:  3
+		},
+		shadowOpacity:  0.15,
+		shadowRadius:  5,
+		shadowColor:  '#1D2660',
+		elevation:  5
+	},
+		btn: {
+		width:  '100%',
+		marginTop:  10,
+		backgroundColor:  COLORS['brand-01']
+	}
+})
+```
+
+  
+
+## License
+
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ index.js
@@ -0,0 +1,289 @@
+import React, { useEffect, useState } from 'react'
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native'
+
+const ButtonItem = ({
+  activeOpacity,
+  onButtonPress,
+  buttonSize = 60,
+  text,
+  customComponent,
+  customViewStyle,
+  accessible,
+  accessibilityLabel,
+  disabled,
+  customTextStyle
+}) => {
+  return (
+    <TouchableOpacity
+      accessible={accessible}
+      accessibilityRole="keyboardkey"
+      accessibilityLabel={customComponent !== undefined ? accessibilityLabel : text}
+      activeOpacity={activeOpacity}
+      disabled={disabled}
+      style={NumpadStyle.buttonContainer}
+      onPress={onButtonPress}>
+      <View
+        style={[
+          NumpadStyle.buttonView,
+          { width: buttonSize, height: buttonSize, borderRadius: buttonSize / 2 },
+          customViewStyle
+        ]}>
+        {customComponent || (
+          <Text style={[NumpadStyle.buttonText, customTextStyle]}>{text}</Text>
+        )}
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const ViewHolder = () => {
+  return <View style={NumpadStyle.buttonContainer} />
+}
+
+const NumericPad = React.forwardRef(
+  (
+    {
+      buttonTextByKey,
+      accessible,
+      style,
+      onValueChange,
+      buttonAreaStyle,
+      activeOpacity,
+      numLength,
+      buttonSize,
+      buttonItemStyle,
+      buttonTextStyle,
+      numericDisabled,
+      allowDecimal,
+      rightBottomButton,
+      rightBottomButtonDisabled,
+      rightBottomButtonSize = 60,
+      rightBottomAccessibilityLabel,
+      rightBottomButtonItemStyle,
+      onRightBottomButtonPress
+    },
+    ref
+  ) => {
+    const [input, setInput] = useState('')
+    ref.current = {
+      clear: () => {
+        if (input.length > 0) {
+          setInput(input.slice(0, -1))
+        }
+      },
+      clearAll: () => {
+        if (input.length > 0) {
+          setInput('')
+        }
+      }
+    }
+
+    const onButtonPressHandle = (key, value) => {
+      // only 1 dot
+      if ((key === 'dot' && input.length < 1) || (key === 'dot' && input.includes('.'))) return
+      if (input.includes('.') && input.substring(input.indexOf('.')).length === 3) return
+      // 2 digits after the dot
+      if (input.length < numLength) {
+        setInput(input + '' + value)
+      }
+    }
+
+    useEffect(() => {
+      if (onValueChange !== undefined) {
+        onValueChange(input)
+      }
+    }, [input])
+
+    return (
+      <View style={[NumpadStyle.container, style]}>
+        <View style={[NumpadStyle.buttonAreaContainer, buttonAreaStyle]}>
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('one', '1')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.one}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('two', '2')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.two}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('three', '3')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.three}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('four', '4')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.four}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('five', '5')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.five}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('six', '6')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.six}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('seven', '7')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.seven}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('eight', '8')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.eight}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('nine', '9')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.nine}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+           {allowDecimal ? (
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('dot', '.')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.dot}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />): (
+            <ViewHolder />
+          )}
+          <ButtonItem
+            disabled={numericDisabled}
+            accessible={accessible}
+            activeOpacity={activeOpacity}
+            onButtonPress={() => onButtonPressHandle('zero', '0')}
+            buttonSize={buttonSize}
+            text={buttonTextByKey.zero}
+            customTextStyle={buttonTextStyle}
+            customViewStyle={buttonItemStyle}
+          />
+          {rightBottomButton !== undefined
+            ? (
+              <ButtonItem
+                disabled={rightBottomButtonDisabled}
+                accessible={accessible}
+                activeOpacity={activeOpacity}
+                accessibilityLabel={rightBottomAccessibilityLabel}
+                onButtonPress={() => onRightBottomButtonPress('right_bottom')}
+                customViewStyle={rightBottomButtonItemStyle}
+                customComponent={rightBottomButton}
+                buttonSize={rightBottomButtonSize}
+              />
+            )
+            : (
+              <ViewHolder />
+            )}
+        </View>
+      </View>
+    )
+  }
+)
+
+NumericPad.defaultProps = {
+  buttonTextByKey: {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+    dot: '.',
+    zero: '0'
+  },
+  accessible: false,
+  onButtonPress: () => {},
+  onRightButtonPress: () => {},
+  style: { paddingVertical: 12 },
+  activeOpacity: 0.9,
+  buttonTextStyle: { color: '#000', fontSize: 30, fontWeight: '400' },
+  rightBottomAccessibilityLabel: 'right_bottom',
+  numericDisabled: false,
+  rightBottomButtonDisabled: false,
+  allowDecimal: true
+}
+
+const NumpadStyle = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  buttonAreaContainer: {
+    alignItems: 'center',
+    alignContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  buttonContainer: {
+    marginBottom: 12,
+    width: '33%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonView: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+})
+
+export default NumericPad
